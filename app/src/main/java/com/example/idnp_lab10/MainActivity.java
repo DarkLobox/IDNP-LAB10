405021df0e8a14fe1a6f43c0228f1de2b18bb491
DarkLobox <57413922+DarkLobox@users.noreply.github.com>
2021-12-07 02:49:18 -0500

    LocationManager Base y Guardado de Archivo

--- app/src/main/java/com/example/idnp_lab10/MainActivity.java
+++ app/src/main/java/com/example/idnp_lab10/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.idnp_lab10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationManager lm;
+    private LocationListener locListenD;
+    private String coordenadas = "";
+    public TextView datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datos = findViewById(R.id.textViewCoordenadas);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (loc != null) {
+            Toast.makeText(this, loc.getLatitude() + "\n " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            coordenadas += loc.getLatitude()+" "+loc.getLongitude()+" "+loc.getAltitude()+"\n";
+        }
+
+        locListenD = new DispLocListener();
+
+        //Modificar minTime y minDistance
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locListenD);
+    }
+    private void guardar(){
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("ruta01.txt",Activity.MODE_PRIVATE));
+            archivo.write(coordenadas);
+            archivo.flush();
+            archivo.close();
+        }catch (IOException e){
+
+        }
+    }
+    private class DispLocListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location != null) {
+                Toast.makeText(getApplicationContext(), location.getLatitude() + "\n " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                coordenadas += location.getLatitude()+" "+location.getLongitude()+" "+location.getAltitude()+"\n";
+                guardar();
+                datos.setText(coordenadas);
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
     }
 }
+
